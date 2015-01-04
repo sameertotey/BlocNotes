73dc54a7421455e6763aeef785689e9cb7d64e1e
Sameer Totey <sameertotey@Sameers-MacBook-Pro.local>
2015-01-03 20:39:03 -0700

    Initial Commit

---
+++ BlocNotes/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  BlocNotes
+//
+//  Created by Sameer Totey on 1/3/15.
+//  Copyright (c) 2015 Sameer Totey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
